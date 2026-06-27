7ffcd062521f460f2408b54cc8bb7d4e67935b7d
Lenhazada <gabriellenhardtcontato@gmail.com>
2026-06-27 09:55:14 -0300

    Product

--- src/main/java/com/educandoweb/coursejava/config/TestConfig.java
+++ src/main/java/com/educandoweb/coursejava/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.coursejava.entities.Category;
 import com.educandoweb.coursejava.entities.Order;
+import com.educandoweb.coursejava.entities.Product;
 import com.educandoweb.coursejava.entities.User;
 import com.educandoweb.coursejava.entities.enums.OrderStatus;
 import com.educandoweb.coursejava.repositories.CategoryRepository;
 import com.educandoweb.coursejava.repositories.OrderRepository;
+import com.educandoweb.coursejava.repositories.ProductRepository;
 import com.educandoweb.coursejava.repositories.UserRepository;
 
 @Configuration
@@ -28,20 +30,35 @@ public class TestConfig implements CommandLineRunner{
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+
+	@Autowired
+	private ProductRepository productRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
+		//instantiate new categories
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
+		//instantiate new products
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
 
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		//instantiate new users
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 	
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
+		//instantiate new orders
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAIMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAIMENT, u1);
