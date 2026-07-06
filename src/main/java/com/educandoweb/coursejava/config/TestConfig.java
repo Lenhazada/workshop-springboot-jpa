9ea79ade621e24139f0f44a3860c1d9d98534753
Lenhazada <gabriellenhardtcontato@gmail.com>
2026-07-06 18:46:01 -0300

    Add comments to TestConfig

--- src/main/java/com/educandoweb/coursejava/config/TestConfig.java
+++ src/main/java/com/educandoweb/coursejava/config/TestConfig.java
@@ -25,6 +25,7 @@ import com.educandoweb.coursejava.repositories.UserRepository;
 @Profile("test")
 public class TestConfig implements CommandLineRunner{
 	
+	// Repositories used to populate the database
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -40,16 +41,22 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private OrderItemRepository orderItemRepository;
 	
+	
+    /**
+     * Populates the database with sample data
+     * when the application starts.
+     */
 	@Override
 	public void run(String... args) throws Exception {
-		//instantiate new categories
+		
+		// Create sample categories
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
-		//instantiate new products
+		// Create sample products
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
@@ -69,20 +76,20 @@ public class TestConfig implements CommandLineRunner{
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 
-		//instantiate new users
+		// Create sample users
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 	
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
-		//instantiate new orders
+		// Create sample orders
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAIMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAIMENT, u1);
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
-		//instantiate new order items 
+		// Create order items 
 		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
@@ -90,7 +97,7 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
-		//instantiate a new payment
+		// Associate a payment with an order
 		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
 		o1.setPayment(pay1);
 		
